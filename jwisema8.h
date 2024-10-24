48590930fdf4d85578684315d999b5786b2bffd1
Justin Wiseman <145676332+aR3V3NGE@users.noreply.github.com>
2024-10-23 20:21:09 -0400

    Update jwisema8.h

--- jwisema8.h
+++ jwisema8.h
@@ -1,1 +1,21 @@
-// Justin's section of BlockAndRoll
+/* Program Name: justin.h
+*  Student Name: Justin Wiseman
+*  Net ID: jwisema8
+*  Student ID: 000-66-6873
+*  Program Description: This header file contains the initialization of Justin's workload for the Tetris team.
+*  */
+
+#include <vector>
+
+class Tetris{
+    public: 
+        Tetris();
+        ~Tetris();
+        void display();
+        vector<int> pieceFall();
+        void music();
+
+    private:
+        int leaderboard();
+        void clearLeaderboard();
+}
